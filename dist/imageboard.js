e7521de0578e11fde7c11a48049e494f13ff7f23
jywarren <jeff@unterbahn.com>
2017-01-01 16:10:50 -0500

    initial commit

---
+++ dist/imageboard.js
@@ -0,0 +1,22 @@
+(function(exports){
+
+  if (typeof module === 'undefined')
+    exports = ImageBoard;
+  else 
+    module.exports = ImageBoard;
+
+})(typeof exports === 'undefined'? this['ImageBoard']={}: exports);
+
+var ImageBoard = function ImageBoard(options){
+
+}
+
+
+
+
+
+jQuery(document).ready(function($) {
+
+  var imageSelect = imageselect({ selector: '#drop' });
+
+});
