74f1f92910e4fc72f4d034651966f9ae939cb1e0
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-06-29 22:18:23 +0530

    Buildfix

--- test/image-manip.js
+++ test/image-manip.js
@@ -6,7 +6,6 @@ var test = require('tape');
 // http://stackoverflow.com/questions/21358015/error-jquery-requires-a-window-with-a-document#25622933
 
 require('../src/ImageSequencer.js');
-pixelmatch = require('@schornio/pixelmatch');
 
 var sequencer = ImageSequencer({ ui: "none" });
 var image = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAQABADASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAABgj/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABykX//Z";
@@ -16,7 +15,6 @@ sequencer.loadImages({images:{
 }, callback:function(){}});
 
 sequencer.addSteps("test1", ['do-nothing-pix','invert','invert']);
-sequencer.addSteps("test2", ['']);
 sequencer.run();
 
 test("Inverted image isn't identical", function (t) {
@@ -27,8 +25,4 @@ test("Inverted image isn't identical", function (t) {
 test("Twice inverted image is identical to original image", function (t) {
   t.equal(sequencer.images.test1.steps[1].output.src, sequencer.images.test1.steps[3].output.src);
   t.end();
-});
-
-test("", function(t) {
-
 });
