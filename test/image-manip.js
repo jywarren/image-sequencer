1117279d441d9f056f121995ecf2a586b7da8f11
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-07-25 01:47:23 +0530

    Added a test to check support for PNG and GIF

--- test/image-manip.js
+++ test/image-manip.js
@@ -8,7 +8,11 @@ var test = require('tape');
 require('../src/ImageSequencer.js');
 
 var sequencer = ImageSequencer({ ui: "none" });
+
+//require image files as DataURLs so they can be tested alike on browser and Node.
 var image = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAQABADASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAABgj/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABykX//Z";
+var test_png = require('../examples/test.png.js');
+var test_gif = require('../examples/test.gif.js');
 sequencer.loadImages(image);
 
 sequencer.addSteps(['do-nothing-pix','invert','invert']);
@@ -22,4 +26,18 @@ test("Inverted image isn't identical", function (t) {
 test("Twice inverted image is identical to original image", function (t) {
   t.equal(sequencer.images.image1.steps[1].output.src, sequencer.images.image1.steps[3].output.src);
   t.end();
+});
+
+test("PixelManipulation works for PNG images :: setup", function (t) {
+  sequencer.loadImages(test_png).addSteps('invert').run(function(out){
+    t.equal(1,1)
+    t.end();
+  });
+});
+
+test("PixelManipulation works for GIF images :: setup", function (t) {
+  sequencer.loadImages(test_gif).addSteps('invert').run(function(out){
+    t.equal(1,1)
+    t.end();
+  });
 });
