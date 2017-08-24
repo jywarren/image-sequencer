38975791caeb17f095009eac76ccd13456c67206
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-08-24 20:13:58 +0530

    Basic CLI

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-console.log('\x1b[31m%s\x1b[0m',"This is the output of the module");
+#!/usr/bin/env node
+
 require('./src/ImageSequencer');
-sequencer = ImageSequencer();
-sequencer.loadImages({images:{red:'examples/images/red.jpg'},callback:function(){
-  sequencer.addSteps(['invert','ndvi-red','invert']);
-  sequencer.removeSteps(1);
-  sequencer.insertSteps({
-    red: [{index: -1, name: 'invert', o:{}}]
-  });
+sequencer = ImageSequencer({ui: false});
+
+var program = require('commander');
+
+function exit(message) {
+  console.log(message);
+  process.exit(1);
+}
+
+program
+  .version('0.1.0')
+  .option('-i, --image [PATH/URL]', 'Input image URL')
+  .option('-s, --step [step-name]', 'Name of the step to be added.')
+  .option('-o, --output [PATH]', 'Directory where output will be stored.')
+  .parse(process.argv);
+
+if(!program.image) exit("Can't read file.")
+
+require('fs').access(program.image, function(err){
+  if(err) exit("Can't read file.")
+});
+
+if(!program.step || !sequencer.modulesInfo().hasOwnProperty(program.step))
+  exit("Please name a valid step.");
+
+program.output = program.output || "./output/";
+
+sequencer.loadImages(program.image,function(){
+  sequencer.addSteps(program.step);
   sequencer.run(function(){
-    sequencer.exportBin('output2');
+    sequencer.exportBin(program.output);
+    console.log("Files will be exported to \""+program.output+"\"");
   });
-}});
+});
