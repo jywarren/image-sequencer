ca9f986d31862806371b4073dd94159ea64a8cda
jywarren <jeff@unterbahn.com>
2018-06-03 12:12:53 -0600

    test import string with no configs

--- test/modules/import-export.js
+++ test/modules/import-export.js
@@ -12,6 +12,7 @@ test('toString() and stepToString() return the step/steps in string format',func
   t.equal(sequencer.stepToString(sequencer.steps[1]),"channel(channel:green)","stepToString works");
   t.end();
 });
+
 test('importStringtoJson() and importStringtoJsonStep() return the step/steps from a string',function(t){
   t.deepEqual(sequencer.importStringtoJson('channel(channel:green),invert(),blend(blend:function(r1%2C%20g1%2C%20b1%2C%20a1%2C%20r2%2C%20g2%2C%20b2%2C%20a2)%20%7B%20return%20%5B%20r1%2C%20g2%2C%20b2%2C%20a2%20%5D%20%7D)'),[
     { name: 'channel', options: { channel: 'green' } },
@@ -19,5 +20,15 @@ test('importStringtoJson() and importStringtoJsonStep() return the step/steps fr
     { name: 'blend', options: { blend: 'function(r1, g1, b1, a1, r2, g2, b2, a2) { return [ r1, g2, b2, a2 ] }' } }
   ]);
   t.deepEqual(sequencer.importStringtoJsonStep("channel(channel:green)"),{ name: 'channel', options: { channel: 'green)' } });
+  t.end();
+});
+
+test('importStringtoJson() and importStringtoJsonStep() return the step/steps from a string without configs, using defaults',function(t){
+  t.deepEqual(sequencer.importStringtoJson('green,blur,invert,blend'),[
+    { name: 'channel', options: { channel: 'green' } },
+    { name: 'blur', options: { blur: '2' } },
+    { name: 'invert', options: {} },
+    { name: 'blend', options: { blend: 'function(r1, g1, b1, a1, r2, g2, b2, a2) { return [ r1, g2, b2, a2 ] }' } }
+  ]);
   t.end();
 });
