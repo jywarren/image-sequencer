76826d431db6f059be495a77c135ced423fc223b
puneet bindal <puneetbindal8989@gmail.com>
2018-09-30 18:43:51 -0700

    refracted defaultHtmlStepUi.js (#362)

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -155,18 +155,14 @@ function DefaultHtmlStepUi(_sequencer, options) {
       var inputs = _sequencer.modulesInfo(step.name).inputs;
       var outputs = _sequencer.modulesInfo(step.name).outputs;
       for (var i in inputs) {
-        if (
-          step.options[i] !== undefined &&
-          inputs[i].type.toLowerCase() === "input"
-        )
-          step.ui.querySelector('div[name="' + i + '"] input').value =
-            step.options[i];
-        if (
-          step.options[i] !== undefined &&
-          inputs[i].type.toLowerCase() === "select"
-        )
-          step.ui.querySelector('div[name="' + i + '"] select').value =
-            step.options[i];
+        if (step.options[i] !== undefined) {
+            if (inputs[i].type.toLowerCase() === "input")
+                step.ui.querySelector('div[name="' + i + '"] input').value =
+                step.options[i];
+            if (inputs[i].type.toLowerCase() === "select")
+                step.ui.querySelector('div[name="' + i + '"] select').value =
+                step.options[i];
+}
       }
       for (var i in outputs) {
         if (step[i] !== undefined)
