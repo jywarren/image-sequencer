98c71b18a8c86646f0cdf95c11e1e3b2a8d36edb
Jeffrey Warren <jeff@unterbahn.com>
2018-11-20 22:53:38 -0500

    Add manifest.json and cache static assets for offline use (#331) (#486)
    * Add manifest.json
    
    * cache static assets for offline use
    
    * update cache
    
    * add meta theme-color and change static files to be cache
    
    * cache the files on network request
    
    * caching on first run
    
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>
    
    * add a button to clear cache
    
    * add styling to clear cache link

--- examples/demo.js
+++ examples/demo.js
@@ -132,4 +132,40 @@ window.onload = function() {
       step.options.step.imgElement.src = reader.result;
     }
   });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/examples/' })
+      .then(function(registration) {
+        const installingWorker = registration.installing;
+        installingWorker.onstatechange = () => {
+          console.log(installingWorker)
+          if (installingWorker.state === 'installed') {
+            location.reload();
+          }
+        }
+        console.log('Registration successful, scope is:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service worker registration failed, error:', error);
+      });
+  }
+
+  if ('serviceWorker' in navigator) {
+        caches.keys().then(function(cacheNames) {
+          cacheNames.forEach(function(cacheName) {
+            $("#clear-cache").append(" " + cacheName);
+          });
+        });
+      }
+
+  $("#clear-cache").click(function() {
+      if ('serviceWorker' in navigator) {
+        caches.keys().then(function(cacheNames) {
+          cacheNames.forEach(function(cacheName) {
+            caches.delete(cacheName);
+          });
+        });
+      }
+  location.reload();
+  });
 };
