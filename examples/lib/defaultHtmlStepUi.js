ef4a4d305598b69e1be8093450c6fc2604a583a1
Vibhor Gupta <vibhordelgupta@gmail.com>
2019-01-07 20:10:53 +0530

    removed crop div (#623)

--- examples/lib/defaultHtmlStepUi.js
+++ examples/lib/defaultHtmlStepUi.js
@@ -174,7 +174,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
       if (stepOptions.index == _sequencer.images.image1.steps.length) {
         stepsEl.appendChild(step.ui);
         $("#steps .container:nth-last-child(1) .insert-step").prop('disabled',true);
-        if($("#steps .container:nth-last-child(2)")) 
+        if($("#steps .container:nth-last-child(2)"))
         $("#steps .container:nth-last-child(2) .insert-step").prop('disabled',false);
       } else {
         stepsEl.insertBefore(step.ui, $(stepsEl).children()[stepOptions.index]);
@@ -183,7 +183,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
     else {
       $("#load-image").append(step.ui);
     }
-    
+
     $('input[type="range"]').on('input', function() {
         $(this).next().html($(this).val());
     })
@@ -241,6 +241,7 @@ function DefaultHtmlStepUi(_sequencer, options) {
   function onRemove(step) {
     step.ui.remove();
     $("#steps .container:nth-last-child(1) .insert-step").prop('disabled',true);
+    $('div[class^=imgareaselect-]').remove();
   }
 
   function getPreview() {
