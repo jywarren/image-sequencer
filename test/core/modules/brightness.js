57e4849d355c280d8c50949b7d7298ef98b08127
Vibhor Gupta <vibhordelgupta@gmail.com>
2019-02-05 22:01:13 +0530

    Add: Module tests (#748)
    * WIP
    
    * module testing harness
    
    * adjustments

--- test/core/modules/brightness.js
+++ test/core/modules/brightness.js
@@ -1,22 +1,41 @@
 var test = require('tape');
+var base64Img = require('base64-img');
+var looksSame = require('looks-same');
+
 require('../../../src/ImageSequencer.js');
 
-var sequencer1 = ImageSequencer({ ui: false });
-var options = {brightness : 50};
-var red = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAQABADASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAABgj/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABykX//Z";
+var sequencer = ImageSequencer({ ui: false });
+var options = {brightness : 1};
+var target = 'test_outputs';
+var red = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABlBMVEX+AAD///+KQee0AAAAAWJLR0QB/wIt3gAAAAd0SU1FB+EGHRIVAvrm6EMAAAAMSURBVAjXY2AgDQAAADAAAceqhY4AAAAldEVYdGRhdGU6Y3JlYXRlADIwMTctMDYtMjlUMTg6MjE6MDIrMDI6MDDGD83DAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE3LTA2LTI5VDE4OjIxOjAyKzAyOjAwt1J1fwAAAABJRU5ErkJggg==";
+var benchmark = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAAAAAA6mKC9AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAd2KE6QAAAAHdElNRQfjAgULAir0wiRrAAAADUlEQVQY02NgGAXIAAABEAAB7JfjegAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxOS0wMi0wNVQxMTowMjo0MiswMjowMCdP+X4AAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTktMDItMDVUMTE6MDI6NDIrMDI6MDBWEkHCAAAAAElFTkSuQmCC";
 
 // Test 1 to check brightness module is getting loaded
 test('Load brightness module', function(t) {
-    sequencer1.loadImages('image1', red);
-    sequencer1.addSteps('brightness', options);
-    t.equal(sequencer1.images.image1.steps[1].options.name, "brightness", "Brightness module is getting loaded.");
-    t.end();
+  sequencer.loadImages('test', red);
+  sequencer.addSteps('brightness', options);
+  t.equal(sequencer.images.test.steps[1].options.name, 'brightness', 'Brightness module is getting loaded');
+  t.end();
 });
 
 // Test 2 to check options are correct
 test('Check Options', function(t) {
-    sequencer1.loadImages('image1', red);
-    sequencer1.addSteps('brightness', options);
-    t.equal(sequencer1.images.image1.steps[1].options.brightness, 50, "Options are correct");
-    t.end();
+  t.equal(sequencer.images.test.steps[1].options.brightness, 1, 'Options are correct');
+  t.end();
 });
+
+// Test 3 to check brightness module works as expected
+test('Brightness module works correctly', function(t) {
+  sequencer.run({ mode: 'test' }, function(out) {
+    var result = sequencer.images.test.steps[1].output.src
+    base64Img.imgSync(result, target, 'result')
+    base64Img.imgSync(benchmark, target, 'benchmark')
+    result = './test_outputs/result.png'
+    benchmark = './test_outputs/benchmark.png'
+    looksSame(result, benchmark, function(err, res) {
+      if (err) console.log(err)
+      t.equal(res.equal, true)
+      t.end()
+    })
+  })
+})
---
+++ test/core/modules/invert.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var test = require('tape');
+var looksSame = require('looks-same');
+var DataURItoBuffer = require('data-uri-to-buffer');
+var base64Img = require('base64-img');
+
+// We should only test headless code here.
+// http://stackoverflow.com/questions/21358015/error-jquery-requires-a-window-with-a-document#25622933
+
+require('../../../src/ImageSequencer.js');
+
+//require image files as DataURLs so they can be tested alike on browser and Node.
+var sequencer = ImageSequencer({ ui: false });
+var target = 'test_outputs'
+
+var red = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABlBMVEX+AAD///+KQee0AAAAAWJLR0QB/wIt3gAAAAd0SU1FB+EGHRIVAvrm6EMAAAAMSURBVAjXY2AgDQAAADAAAceqhY4AAAAldEVYdGRhdGU6Y3JlYXRlADIwMTctMDYtMjlUMTg6MjE6MDIrMDI6MDDGD83DAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE3LTA2LTI5VDE4OjIxOjAyKzAyOjAwt1J1fwAAAABJRU5ErkJggg==";
+var invert = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAHklEQVQ4T2Nk/P//PwMFgHHUAIbRMGAYDQOGYREGAPACL/GqhhwEAAAAAElFTkSuQmCC";
+
+//Tests for Invert module
+
+test("Load invert module", function(t) {
+  sequencer.loadImages('test', red);
+  t.equal(sequencer.images.test.steps.length, 1,'Image loaded')
+  sequencer.addSteps('test','invert');
+  t.equal(sequencer.images.test.steps[1].options.name, 'invert', 'Invert step added')
+  t.end()
+})
+
+test("Inverted image isn't identical", function(t) {
+  sequencer.run({ mode: 'test' }, function(out) {
+    var input = sequencer.images.test.steps[0].output.src;
+    var output = sequencer.images.test.steps[1].output.src;
+    input = DataURItoBuffer(input);
+    output = DataURItoBuffer(output);
+    t.notEqual(input,output,'Not equal')
+    t.end()
+  })
+});
+
+test("Twice inverted image is identical to original image", function(t) {
+  sequencer.addSteps('test','invert');
+  sequencer.run({ mode: 'test' }, function(out) {
+    var input = sequencer.images.test.steps[0].output.src
+    var output = sequencer.images.test.steps[2].output.src
+    base64Img.imgSync(input, target, 'input')
+    base64Img.imgSync(output, target, 'output')
+    input = './test_outputs/input.png'
+    output = './test_outputs/output.png'
+    looksSame(input, output, function(err, res) {
+      if (err) console.log(err)
+      t.equal(res.equal, true)
+      t.end();
+    });
+  })
+});
+
+test("Invert module produces correct output", function(t) {
+  sequencer.run({ mode: 'test' }, function(out) {
+    var result = sequencer.images.test.steps[1].output.src
+    var benchmark = invert
+    base64Img.imgSync(result, target, 'result')
+    base64Img.imgSync(benchmark, target, 'benchmark')
+    result = './test_outputs/result.png'
+    benchmark = './test_outputs/benchmark.png'
+    looksSame(result, benchmark, function(err, res) {
+      if (err) console.log(err)
+      t.equal(res.equal, true)
+      t.end()
+    })
+  })
+})
