2d8bb53402bba618613dddcbd0dd53f52ad6bd43
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2019-02-08 21:51:24 +0530

    Used `pixels.set` To Refactor DrawRectangle (#690)
    * update dist
    
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>
    
    dist update
    
    Revert "dist update"
    
    This reverts commit 9ee2a987e8f978961656ae8f71f6e6702bbbd30d.
    
    * use pixel.set for drawRectangle
    
    * chnage thickness

--- src/modules/DrawRectangle/DrawRectangle.js
+++ src/modules/DrawRectangle/DrawRectangle.js
@@ -5,33 +5,26 @@ module.exports = exports = function(pixels, options){
 	  oy = Number(options.startingY),
 	  iw = pixels.shape[0],
 	  ih = pixels.shape[1],
-	  ex =  options.endX = Number(options.endX) || iw - 1,
-	  ey = options.endY = Number(options.endY) || ih - 1,
     thickness = Number(options.thickness) || 1,
+	  ex =  options.endX = Number(options.endX) - thickness || iw - 1,
+	  ey = options.endY = Number(options.endY) -thickness || ih - 1,
     color = options.color || "0 0 0 255";
+    color = color.split(" ");
 
-  color = color.split(" ");
-
-  var drawSide = function(startY, startX, endY, endX, yIncrement, xIncrement, sign, incrementFactor = 1){
-    for(var i = 0; i<thickness; i++){
-      for(
-        var n = (startY + i*yIncrement*sign)*4*iw + 4*(startX + i*xIncrement*sign);
-        n <= (endY + i*yIncrement*sign)*4*iw + 4*(endX + i*xIncrement*sign);
-        n = n+4*(incrementFactor)
-      )
-      {
-        pixels.data[n] = color[0];
-        pixels.data[n+1] = color[1];
-        pixels.data[n+2] = color[2];
-        pixels.data[n+3] = color[3];
+  var drawSide = function(startX, startY, endX, endY){
+    for (var n=startX; n <= endX+thickness; n++){
+      for (var k=startY; k <= endY+thickness; k++){
+        pixels.set(n, k, 0, color[0]);
+        pixels.set(n, k, 1, color[1]);
+        pixels.set(n, k, 2, color[2]);
+        pixels.set(n, k, 3, color[3]);
       }
     }
   }
 
-  drawSide(oy, ox, oy, ex, 1, 0, 1); // Top
-  drawSide(ey, ox, ey, ex, 1, 0, -1); // Bottom
-  drawSide(oy, ox, ey, ox, 0, 1, 1, iw); // Left
-  drawSide(oy, ex, ey, ex, 0, 1, -1, iw); // Right
-
+  drawSide(ox, oy, ox, ey); // Left
+  drawSide(ex, oy, ex, ey); // Right
+  drawSide(ox, oy, ex, oy); // Top
+  drawSide(ox, ey, ex, ey); // Bottom
   return pixels;
 }
