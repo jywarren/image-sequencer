c7980c1158ef74cdb7e9b7833b1236860da5af0d
aashna27 <aashna.mittal27@gmail.com>
2019-08-19 20:29:09 +0530

    grid-overlay for negetive input and 0 (#1205)

--- src/modules/GridOverlay/GridOverlay.js
+++ src/modules/GridOverlay/GridOverlay.js
@@ -2,8 +2,15 @@ module.exports = exports = function(pixels, options){
   var defaults = require('./../../util/getDefaults.js')(require('./info.json'));
   const pixelSetter = require('../../util/pixelSetter.js');
 
-  options.x = Number(options.x) || defaults.x;
-  options.y = Number(options.y) || defaults.y;
+  if(Number(options.x)==0){
+    options.x = 1;
+  } 
+  if( Number(options.y)==0) {
+    options.y =1;
+  }
+
+  options.x = Math.abs(Number(options.x)) || defaults.x;
+  options.y = Math.abs(Number(options.y)) || defaults.y;
   color = options.color || defaults.color;
   color = color.substring(color.indexOf('(') + 1, color.length - 1); // extract only the values from rgba(_,_,_,_)
   color = color.split(',');
