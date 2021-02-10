eb81b7de4b331dc32684e90ee9898520c80c14e4
Tanish <64203208+tanish14@users.noreply.github.com>
2021-02-10 20:49:36 +0530

    standardised util functions comments (#1805)
    * standardised util functions comments
    
    * Update pixelSetter.js
    
    * Update pixelSetter.js
    
    * Update pixelSetter.js
    
    Co-authored-by: Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>

--- src/util/getDefaults.js
+++ src/util/getDefaults.js
@@ -1,3 +1,4 @@
+/** Parses the defaults and gets the input which is available. */
 module.exports = function(info){
   var defaults = {};
   for (var key in info.inputs) {
--- src/util/pixelSetter.js
+++ src/util/pixelSetter.js
@@ -1,3 +1,10 @@
+/**  
+ * @param {number} x x-coordinate.
+ * @param {number} y y-coordinate.
+ * @param {object} value array [r, g, b, a]
+ * @param {object} pixels NDarray of pixels.
+ * @description Sets the pixels from 0 through length of value. 
+*/
 module.exports = function(x, y, value, pixels){
   for(let i = 0; i < value.length; i++){
     pixels.set(x, y, i, value[i]);
